f485e5fbc099eec8fd7788a38dea852f629efc89
Izumiko <yosoro@outlook.com>
2021-08-19 14:32:41 +0800

    fix

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ sudo apt-get update && sudo apt-get install --yes --no-install-recommends ca-cer
 # download syno toolkit
 curl --location "https://sourceforge.net/projects/dsgpl/files/toolkit/DSM7.0/ds.apollolake-7.0.dev.txz/download" --output ds.apollolake-7.0.dev.txz
 
-tar -xvf ds.apollolake-7.0.dev.txz usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
+tar -xf ds.apollolake-7.0.dev.txz usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
 
 # build redpill-lkm
 cd redpill-lkm
 make LINUX_SRC=../usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
 cd ..
-read -a KVERS <<< "$$(sudo modinfo --field=vermagic redpill-lkm/redpill.ko)" && cp -f redpill-lkm/redpill.ko redpill-load/ext/rp-lkm/redpill-linux-v$${KVERS[0]}.ko
+read -a KVERS <<< "$(sudo modinfo --field=vermagic redpill-lkm/redpill.ko)" && cp -fv redpill-lkm/redpill.ko redpill-load/ext/rp-lkm/redpill-linux-v${KVERS[0]}.ko || exit 1
 
 # build redpill-load
 cp user_config.json redpill-load/
